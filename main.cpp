d3a04c569e8e21ecb24e271be3c27268fdb29adc
unknown <you@example.com>
2018-09-30 11:06:11 -0500

    PE5

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int n = 1; n!=0; n++){
+        if (n%2==0 && n%3==0 && n%4==0 && n%5==0 && n%6==0 && n%7==0 && n%8==0 && n%9==0 && n%10==0 && n%11==0 && n%12==0 && n%13==0 && n%14==0 && n%15==0 && n%16==0 && n%17==0 && n%18==0 && n%19==0){
+            cout << n << endl;
+            return 0;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+/*
+{
+    int a ;
+    for(int n = 1; n!=0; n++)
+        while ( a <=20)
+            if(n%a==0){
+                cout << n<< endl ;
+            }
+
+
+
+    return 0;
+
+
+}*/
+
+
